04b2547515cda25ab255872d51a86fdfe3bbc82d
martandy <zhiiqiang.chen@gmail.com>
2025-10-12 16:37:05 +0700

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App: React.FC =  () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+  mode: "development",
+};
